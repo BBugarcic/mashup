9c5cadbe670d74e9bd6de8f8af1606c556495548
Branislav Bugarcic <bugarcic@gmail.com>
2015-10-23 23:14:46 +0200

    solution, version 2 improved search

--- public/search.php
+++ public/search.php
@@ -3,29 +3,26 @@
     require(__DIR__ . "/../includes/config.php");
 
 	// decode the URL ($_GET["geo"])
-	// breaking url-strng into an array containing keywords
-	// removing whitespaces from the end and beginning of words (strings)
-	$param = array_map('trim', explode(",", urldecode($_GET["geo"])));
+	$url = urldecode($_GET["geo"]);
 	
-	// removing "US" parametar from key, every address is in US
-	if (($us = array_search("US", $param)) !== false)
-	{
-		unset($param[$us]);
-	}
-	
+	// replacing commas with spaces, and double spacies with one space
+	$words = str_replace("  ", " ", (str_replace(",", " ", $url)));
+
+	$param = explode(" ", $words);
+
 	// building sql query
 	$sql = "SELECT * FROM places WHERE ";
 	
 	for ($i = 0, $j = sizeof($param); $i < $j; $i++)
 	{ 
-		// if key is numeric, assume it is a postal code
+		// if string is numeric, assume it is a postal code
 		if (is_numeric($param[$i]))
 		{
 			$sql .= "(postal_code='$param[$i]')";		
 		}   	
 		else
 		{		
-			$sql .= "(place_name='$param[$i]' OR admin_name1='$param[$i]' OR admin_code1 LIKE '$param[$i]')";
+			$sql .= "(place_name LIKE '%$param[$i]%' OR admin_name1 LIKE '%$param[$i]%' OR admin_code1='$param[$i]')";
 	
 			if ($i < $j - 1) 
 			{
@@ -33,43 +30,43 @@
 			}		
 		}
 	}
-	
-	$places = query($sql);
-	
-	// if user do not enter comma after first word
-	// breaking elements of parametar array to new array
-	if (empty($places))
-	{	
-		// building sql query with new parameters
-		$sql = "SELECT * FROM places WHERE ";
 
-		foreach($param as $key => $val)	
-		{	
-			$new_param = explode(" ", $val);
-			
-			//for ($i = 0, $j = sizeof($new_param); $i < $j, $i++)
-			//{
+	// take data from database
+	$search = query($sql);
 	
-				for ($i = 0, $j = sizeof($new_param); $i < $j; $i++)
-				{ 
-					// if key is numeric, assume it is a postal code
-					if (is_numeric($new_param[$i]))
-					{
-						$sql .= "(postal_code='$new_param[$i]')";		
-					}   	
-					else
+	$param_size = sizeof($param);
+	$places = [];
+	
+	// iterate through search array
+	// comparing data with parametars
+	// counting number of matching ($match)
+	// adding elements into new array ($places)
+	foreach($search as $rows)
+	{	
+		$match = 0;	
+		foreach ($rows as $column => $val)
+		{								
+			for ($i = $param_size - 1; $i >= 0; $i--)
+			{					
+				if (strcmp($param[$i], $val) === 0)
+				{
+					$match++;
+					if ($match === $param_size)
 					{		
-						$sql .= "(place_name='$new_param[$i]' OR admin_name1='$new_param[$i]' OR admin_code1 LIKE '$new_param[$i]')";
-	
-						if ($i < $j - 1) 
-						{
-							$sql .= " AND ";
-						}		
+						$places[] = $rows;
+					}					
+				}			
+				if (strpos($val, $param[$i]. " ") !== 0)
+				{
+					$match++;
+					if ($match === $param_size)
+					{		
+						$places[] = $rows;
 					}
 				}
-		}		
 				
-		$places = query($sql);
+			}
+		}
 	}
 
     // output places as JSON (pretty-printed for debugging convenience)
